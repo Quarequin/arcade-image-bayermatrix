1f7a229972112e353c0f63513f9ca2ac1159abc6
Phat Quarequin <naphatism@hotmail.com>
2026-07-06 10:58:21 +0700

    Update api.d.ts

--- api.d.ts
+++ api.d.ts
@@ -11,13 +11,11 @@ interface Image {
      * @param opacity as 0-255 for pseudo-opacity
      * @param precomputed bayer-matrix size as enum
      */
-    //% blockId=image_draw_opacity_bayer
-    //% block="$this=image_picker draw matrix $from=image_picker at x $x y $y opacity $opacity|| from $level"
-    //% blockNamespace=images
-    //% inlineInputMode=inline
+    //% blockNamespace=images inlineInputMode=inline blockId=image_draw_opacity_bayer
+    //% block="%this=variables_get draw matrix %from=image_picker at x %x y %y opacity $opacity|| from %level"
     //% group=Drawing
-    //% weight=35
     //% opacity.min=0 opacity.max=255 opacity.defl="128"
+    //% this.defl=picture
     //% helper=imageDrawBayerImage
     drawBayerImage(from: Image, x: number, y: number, opacity: number, level?: image.BayerSize): void;
 
