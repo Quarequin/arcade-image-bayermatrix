cb9ce4beb241a46d16a2d4d18684759a5a0f0e0f
Phat Quarequin <naphatism@hotmail.com>
2026-07-12 20:06:43 +0700

    Add BayerSize enum to image namespace

---
+++ enums.d.ts
@@ -0,0 +1,11 @@
+
+declare namespace image {
+    export enum BayerSize {
+        //% block="bayer 4x4"
+        x4 = 0x3,
+        //% block="bayer 8x8"
+        x8 = 0x7,
+        //% block="bayer 16x16"
+        x16 = 0xf,
+    }
+}
