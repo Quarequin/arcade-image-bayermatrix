f4e029ef07d1da9d28bdf8ddf607f446b63ad7cf
Phat Quarequin <naphatism@hotmail.com>
2026-07-12 20:05:47 +0700

    Update and rename imgbayer.d.ts to image.d.ts

--- image.d.ts
+++ image.d.ts
@@ -1,33 +1,4 @@
 
-declare namespace image {
-    export enum BayerSize {
-        //% block="bayer 4x4"
-        x4 = 0x3,
-        //% block="bayer 8x8"
-        x8 = 0x7,
-        //% block="bayer 16x16"
-        x16 = 0xf,
-    }
-}
-
-declare namespace helpers {
-
-    const BAYER4X4_DATA: Buffer;
-    const BAYER8X8_DATA: Buffer;
-    const BAYER16X16_DATA: Buffer;
-    // bayer_drawcore
-    // - bayer_drawcore's init (section.data like)
-    let bx: int32, by: int32, b: uint8,
-        bs: int8, bn: int8, ibx: int32, iby: int32;
-    let frowBuf: Buffer, trowBuf: Buffer, curBayer: Buffer;
-    let bayer_drawcore_inuse: boolean;
-    // - reused function (not referense from makecode arcade bulit-in function)
-    const local_neg_abs: (n: number) => number;
-    // end bayer_drawcore
-    function bayer_drawcore(to: Image, from: Image, x: number, y: number, opacity: number, level: image.BayerSize, transparent: boolean): void;
-
-}
-
 declare interface Image {
 
     /**
